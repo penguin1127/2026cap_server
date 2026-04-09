471457cc70179ce565f8f36f6d2afa25adcdc8fd
조성민 <tjdals7071@kyungmin.ac.kr>
2026-04-09 16:10:48 +0900

    백엔드 1차 초기 세팅

---
+++ server/src/main/java/com/expansion/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.expansion.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
